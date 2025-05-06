ad4c0276ceee36863f2dd7ed07b6dcb3c480d91c
tahircanyildiz <tahircanyildiz@hotmail.com>
2025-05-06 10:55:08 +0300

    login eklendi

---
+++ Kullanıcı Yonetimi/login.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://portal.cvus.app/');
+  await page.getByRole('textbox', { name: 'E-posta' }).click();
+  await page.getByRole('textbox', { name: 'E-posta' }).fill('melih.demir@gmail.com');
+  await page.getByRole('textbox', { name: 'E-posta' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Password' }).fill('M');
+  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Melih.demir@12');
+  await page.getByRole('button', { name: 'Giriş Yap' }).click();
+});
