8f1022b4e4230bad5395c7ebf6e2017a208aa2cb
tahircanyildiz <tahircanyildiz@hotmail.com>
2025-05-08 09:15:06 +0300

    ayarlar e2e eklendi

--- ayarlar/mülakat.spec.js
+++ ayarlar/mülakat.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import {login} from '../Kullanıcı Yonetimi/login.spec';
 
-test('test', async ({ page }) => {
+test('mülakat ayarları başarıyla gerçekleşmeli', async ({ page }) => {
 
   login(page)
   await page.getByRole('button', { name: 'Ayarlar' }).click();
